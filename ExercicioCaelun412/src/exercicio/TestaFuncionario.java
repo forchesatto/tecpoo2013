6d25ae0eaafb07e62963fde0fd3d4b58416cad38
André Luiz Forchesatto <andreforchesatto@gmail.com>
2013-03-11 20:15:36 -0300

    Criado IF para comparar instancias

--- ExercicioCaelun412/src/exercicio/TestaFuncionario.java
+++ ExercicioCaelun412/src/exercicio/TestaFuncionario.java
@@ -11,7 +11,8 @@ import exercicio.Funcionario;
  * @author andreforchesatto
  */
 public class TestaFuncionario {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Funcionario f1 = new Funcionario();
 
         f1.nome = "Hugo";
@@ -20,9 +21,23 @@ public class TestaFuncionario {
 
         System.out.println("salario atual:" + f1.salario);
         System.out.println("ganho anual:" + f1.calculaGanhoAnual());
-        
+
         f1.demite();
-        
-        System.out.println("Funcionario demitido?"+f1.estaNaEmpresa);
-   }
+
+        System.out.println("Funcionario demitido?" + f1.estaNaEmpresa);
+
+        Funcionario f2 = new Funcionario();
+        f1.nome = "Danilo";
+        f1.salario = 100;
+
+        Funcionario f3 = new Funcionario();
+        f2.nome = "Danilo";
+        f2.salario = 100;
+
+        if (f2 == f3) {
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
+    }
 }
