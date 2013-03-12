75348f5bdaa9cfcba2937bd67eec0e7d2d681ae5
André Luiz Forchesatto <andreforchesatto@gmail.com>
2013-03-11 22:10:28 -0300

    Adicionado códigos iniciais para desafio fibonacci

---
+++ ExercicioCaelun412/src/exercicio/Fibonacci.java
@@ -0,0 +1,14 @@
+package exercicio;
+
+/**
+ *
+ * @author andreforchesatto
+ */
+public class Fibonacci {
+
+    int calculaFibonacci(int i) {
+        System.out.println("Fazer logica");
+        return 0;
+    }
+    
+}
---
+++ ExercicioCaelun412/src/exercicio/TesteFibonacci.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio;
+
+/**
+ *
+ * @author andreforchesatto
+ */
+public class TesteFibonacci {
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        for (int i = 1; i <= 6; i++) {
+            int resultado = fibonacci.calculaFibonacci(i);
+            System.out.println(resultado);
+        }
+    }
+}
